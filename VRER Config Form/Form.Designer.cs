1f49d09424c7cb9adda502b662071119a01bd4b6
Thijs Hendrickx <thendjes17@gmail.com>
2018-09-10 12:26:53 +0200

    Create basic form application.

---
+++ VRER Config Form/Form.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace VRER_Config_Form
+{
+	partial class Form
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+			this.SuspendLayout();
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(98, 120);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(53, 17);
+			this.checkBox1.TabIndex = 0;
+			this.checkBox1.Text = "Fan 1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(216, 120);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(53, 17);
+			this.checkBox2.TabIndex = 1;
+			this.checkBox2.Text = "Fan 2";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(350, 120);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(53, 17);
+			this.checkBox3.TabIndex = 2;
+			this.checkBox3.Text = "Fan 3";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.AutoSize = true;
+			this.checkBox4.Location = new System.Drawing.Point(476, 120);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(53, 17);
+			this.checkBox4.TabIndex = 3;
+			this.checkBox4.Text = "Fan 4";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(216, 166);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(179, 53);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Voeg timestamp toe";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(248, 70);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 5;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(216, 241);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(179, 46);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Start";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(216, 311);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(179, 46);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "Stop";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(735, 432);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.checkBox4);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.IO.Ports.SerialPort serialPort1;
+	}
+}
+
---
+++ VRER Config Form/Form.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VRER_Config_Form
+{
+	public partial class Form : System.Windows.Forms.Form
+	{
+		public Form()
+		{
+			InitializeComponent();
+			serialPort1.PortName = "COM4";
+			serialPort1.BaudRate = 9600;
+			serialPort1.Open();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			serialPort1.Write(textBox1.Text + '\n' + (checkBox1.Checked ? 1 : 0) + '\n' + (checkBox2.Checked ? 1 : 0) + '\n' + (checkBox3.Checked ? 1 : 0 ) + '\n' + (checkBox4.Checked ? 1 : 0) + '\n');
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			serialPort1.Write("*");
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			serialPort1.Write("-");
+		}
+	}
+}
